a2f9dea66220727bc4837da8038a5a3671d6b7d7
Lucas-OF28 <luuu_of@hotmail.com>
2022-07-14 17:56:17 -0300

    testes

---
+++ 2_FUNDAMENTOS/1_modulo_interno/index.js
@@ -0,0 +1,7 @@
+const meuModulo = require('./meu_modulo')
+const soma = meuModulo.soma
+
+soma(2,6)
+soma(8,10)
+soma(18,2)
+soma(80,5)
---
+++ 2_FUNDAMENTOS/2_export_import/index.js
@@ -0,0 +1,5 @@
+import soma from "./meu_modulo.mjs"
+
+soma(2,5)
+soma(8,9)
+soma(7,10)
